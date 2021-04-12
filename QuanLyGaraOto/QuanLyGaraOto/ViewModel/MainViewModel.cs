5cee128bbe51bda50926f75f555da3243a1c847c
DatNguyen-267 <71223440+DatNguyen-267@users.noreply.github.com>
2021-04-12 22:58:00 +0700

    commit car reception and service window

--- QuanLyGaraOto/QuanLyGaraOto/ViewModel/MainViewModel.cs
+++ QuanLyGaraOto/QuanLyGaraOto/ViewModel/MainViewModel.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLyGaraOto.ViewModel
 {
     public class MainViewModel: BaseViewModel
     {
-
+        public ICommand LoadedWindowCommand { get; set; }
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p)=> { return true; }, (p)=> {
+                ServiceWindow serviceWindow = new ServiceWindow();
+                serviceWindow.ShowDialog();
+            });
+        }
     }
 }
