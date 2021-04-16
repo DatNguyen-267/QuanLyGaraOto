512fa112ef53f26c7eb14b53a7831069da2d13da
DatNguyen-267 <71223440+DatNguyen-267@users.noreply.github.com>
2021-04-16 13:10:38 +0700

    commit

--- QuanLyGaraOto/QuanLyGaraOto/ViewModel/CarReceptionViewModel.cs
+++ QuanLyGaraOto/QuanLyGaraOto/ViewModel/CarReceptionViewModel.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
+using System.Windows.Input;
 namespace QuanLyGaraOto.ViewModel
 {
-    public class CarReceptionViewModel : BaseViewModel
+    public class CarReceptionViewModel :BaseViewModel
     {
+        public ICommand CloseCommand { get; set; }
+        public CarReceptionViewModel()
+        {
+            CloseCommand = new RelayCommand<Window>((p) => true, (p) =>
+            {
+                p.Close();
+            });
 
+        }
     }
 }
