62cdf152616c740318941d8a90c092b40ff25253
DatNguyen-267 <71223440+DatNguyen-267@users.noreply.github.com>
2021-05-18 20:11:14 +0700

    commit

--- query/Create_GaraDB.sql
+++ query/Create_GaraDB.sql
@@ -8,55 +8,54 @@ GO
 ---------------------DON HANG CUA KHACH HANG-----------------------------------
 
 -- Thiet lap danh sach cac hang xe
-create table CarBrand
+create table CARBRAND
 (
     Id int primary key not NULL IDENTITY(1, 1),
-    Name varchar(max) not NULL
+    CarBrand_Name varchar(max) not NULL
 );
 GO
 
 -- Thiet lap danh sach khach hang
-create table Customer
+create table CUSTOMER
 (
     Id int primary key not NULL IDENTITY(1, 1),
-    Name varchar(100) not NULL,
-    Address varchar(max),
-    Telephone varchar(20) not NULL
+    Customer_Name varchar(100) not NULL,
+    Customer_Address varchar(max),
+    Customer_Phone varchar(20) not NULL
 );
 GO
 
 -- Thiet lap danh sach loai tien cong
-create table Pay
+create table PAY
 (
     Id int primary key not NULL IDENTITY(1, 1),
-    Name varchar(1000) not NULL,
-    Price int not NULL
+    Pay_Name varchar(1000) not NULL,
+    Pay_Price int not NULL
 );
 GO
 
 -- Thiet lap danh sach cac dich vu tai gara
 
 -- Thiet lap danh sach vat tu, phu tung
-create table Supply
+create table SUPPLIES
 (
     Id int IDENTITY(1, 1) primary key not NULL,
-    Name varchar(1000) not NULL,
-	DisplayName varchar(1000) not NULL,
-    Price int,
-    Amount int
+    Supply_Name varchar(1000) not NULL,
+    Supply_Price int,
+    Supply_Amount int
 );
 GO
 
 -- Thiet lap trang thai xe
-create table CarStatus
+create table CARSTATUS
 (
     Id int primary key not NULL IDENTITY(1, 1),
-    Name varchar(200) not NULL
+    Status_Name varchar(200) not NULL
 );
 GO
 
 -- Thiet lap phieu tiep nhan xe
-create table CarReception
+create table CARRECEPTION
 (
     Id int primary key not NULL IDENTITY(1, 1),
     IdCustomer int not NULL,
@@ -64,14 +63,14 @@ create table CarReception
     IdBrand int not NULL,
     ReceptionDate date not NULL,
 	IdStatus int not null,
-    constraint FK_CarReception_Customer foreign key (IdCustomer) references Customer(Id),
-    constraint FK_CarReception_CarBrand foreign key (IdBrand) references CarBrand(Id),
-	constraint FK_CarReception_CarStatus foreign key (IdStatus) references CarStatus(Id)
+    constraint FK_CarReception_Customer foreign key (IdCustomer) references CUSTOMER(Id),
+    constraint FK_CarReception_CarBrand foreign key (IdBrand) references CARBRAND(Id),
+	constraint FK_CarReception_CarStatus foreign key (IdStatus) references CARSTATUS(Id)
 );
 GO
 
 -- Thiet lap phieu sua chua
-create table RepairForm
+create table REPAIRFORM
 (
     Id int primary key not NULL IDENTITY(1, 1),
     IdCarReception int not NULL,
@@ -80,44 +79,33 @@ create table RepairForm
 );
 GO
 -- Thiet lap danh sach noi dung sua chua
-create table RepairInfo 
+create table REPAIRINFO 
 (
 	Id int identity(1,1) not null primary key,
 	IdRepairForm int not null,
-	Content varchar(max) not NULL,
+	Content varchar(max),
     IdSupply int,
     SuppliesAmount int,
     IdPay int not NULL,
     TotalMoney int not NULL,
-	constraint FK_RepairInfo_Supply foreign key (IdSupply) references Supply(Id),
+	constraint FK_RepairInfo_Supply foreign key (IdSupply) references Supplies(Id),
     constraint FK_RepairInfo_Pay foreign key (IdPay) references Pay(Id),
 	constraint FK_RepairInfo_RepairForm foreign key (IdRepairForm) references RepairForm(Id)
 )
 GO
--- drop table REPAIR_TICKET;
-
--- Thiet lap danh sach cac xe dang trong qua trinh thu no
---create table CAR_IN_DEBT
---(
---    License_ID int primary key not null,
---    Debt int,
---    constraint FK_CARLIST_LICENSEPLATE foreign key (License_ID) references LICENSE_PLATE(ID)
---);
--- drop table CAR_IN_DEBT;
-
 -- Thiet lap danh sach thong tin cua gara
-create table GaraInfo
+create table GARAINFO
 (
 	Id int primary key identity (1,1) not null,
 	MaxCar int,
 	MaxCarReception int,
-	Phone varchar(20),
-	Email varchar(200),
-	Address varchar(max),
+	GaraInfo_Phone varchar(20),
+	GaraInfo_Email varchar(200),
+	GaraInfo_Address varchar(max),
 );
 Go
 -- Thiet lap danh sach cac hoa don -> doanh so cua thang
-create table Receipt
+create table RECEIPT
 (
 	Id int primary key identity(1,1) not null,
     IdCarReception int not NULL,
@@ -134,35 +122,35 @@ GO
 --------------------NHAN SU CUA GARA--------------------------------
 
 -- Thiet lap danh sach cac role bao gom staff va admin
-create table UserRole
+create table ROLE
 (
 	Id int identity(1,1) primary key,
-	Name nvarchar(max),
+	Role_Name nvarchar(max),
 )
 go
 -- Thiet lap danh sach tai khoan cua nguoi dung
-create table Users
+create table USERS
 (
     Id int primary key not NULL IDENTITY(1, 1),
     UserName varchar(100) not NULL,
     Password varchar(100) not NULL,
     IdRole int not NULL,
-	isActive int,
-	foreign key(IdRole) references UserRole(Id),
+	IsActive int,
+	foreign key(IdRole) references ROLE(Id),
 );
 GO
 -- Thiet lap thong tin cua nguoi dung
-create table UserInfo
+create table USERINFO
 (
     Id int primary key not NULL IDENTITY(1, 1),
 	IdUser int,
-    Name varchar(200) not NULL,
-    Address varchar(max),
-    BirthDate date,
-    Telephone varchar(20) not NULL,
-    CMND varchar(100) not NULL,
+    UserInfo_Name varchar(200) not NULL,
+    UserInfo_Address varchar(max),
+    UserInfo_BirthDate date,
+    UserInfo_Telephone varchar(20) not NULL,
+    UserInfo_CMND varchar(100) not NULL,
     constraint FK_UserInfo_Users foreign key (IdUser) references Users(Id)
 );
 
-insert into UserRole(Name) values ('admin');
-insert into Users(UserName,Password,IdRole) values ('admin','db69fc039dcbd2962cb4d28f5891aae1',1);
+insert into ROLE(Role_Name) values ('admin');
+insert into USERS(UserName,Password,IdRole) values ('admin','db69fc039dcbd2962cb4d28f5891aae1',1);
