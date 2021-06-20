146d37bddd39ecdf7aa959aaa3b997658e437d65
paracetamol-user <huynhanhkiet179@gmail.com>
2021-06-20 22:02:30 +0700

    commit

--- query/SampleData_Import.sql
+++ query/SampleData_Import.sql
@@ -17,3 +17,5 @@ insert into Customer values(N'Hung' , N'TPHCM', '091923921')
 --insert into CAR_STATUS values(N'Đang sửa chữa')
 --insert into CAR_STATUS values(N'Đã sửa chữa xong')
 --insert into CAR_STATUS values(N'Hoàn Thành')
+
+insert into SUPPLIER values(N'Huynh Anh Kiet', '1234012341', 'lfasdkflasjdfla@gmail.com') 
