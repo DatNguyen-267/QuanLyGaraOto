9f943a7ab3d6f0091fd338ff3857eef972c55a75
DatNguyen-267 <71223440+DatNguyen-267@users.noreply.github.com>
2021-06-24 16:12:00 +0700

    commit

--- QuanLyGaraOto/QuanLyGaraOto/ViewModel/ModifyWageViewModel.cs
+++ QuanLyGaraOto/QuanLyGaraOto/ViewModel/ModifyWageViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -50,6 +51,8 @@ namespace QuanLyGaraOto.ViewModel
                 {
                     return false;
                 }
+                Regex regex = new Regex(@"^[0-9]+$");
+                if (!regex.IsMatch((p.txtValue.Text)) && !string.IsNullOrEmpty((p.txtValue.Text))) return false;
                 return true;
             }, (p) =>
             {
